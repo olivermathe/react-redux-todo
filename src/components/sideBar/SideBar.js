4d729a9617d6825d5b65f2a2d4671028f1263671
Matheus Oliveira <olivermathe16@gmail.com>
2018-07-17 21:53:52 -0300

    SwipeableDrawer

--- src/components/sideBar/SideBar.js
+++ src/components/sideBar/SideBar.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import { Drawer } from "@material-ui/core";
+import { SwipeableDrawer } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 import BarList from "./BarList";
 
-import { 
-  fetchSideBarItems, 
-  toogleSideBar 
-} from "../../actions/SideBarActions";
+import { fetchSideBarItems, toogleSideBar } from "../../actions/SideBarActions";
 
 class SideBar extends Component {
+
+  // class config [methods, state]
 
   componentWillMount() {
 
@@ -19,22 +19,45 @@ class SideBar extends Component {
 
   render() {
 
-    const { isOpen, items } = this.props;
+    // variables to 'html' body
+
+    const { isOpen, items, classes } = this.props;
+    
+    const paper = {
+      paper: classes.drawer
+    }
+
+    const modalProps = {
+      keepMounted: true
+    }
+
+    // 'html' body
 
     return (
-      <Drawer open={isOpen} onClose={this.props.toogleSideBar}>
+      <SwipeableDrawer ModalProps={modalProps} classes={paper} open={isOpen} onOpen={this.props.toogleSideBar} onClose={this.props.toogleSideBar}>
         <div tabIndex={0} role="button" onClick={this.props.toogleSideBar}>
           <BarList items={items}/>
         </div>
-      </Drawer>
+      </SwipeableDrawer>
     );
   }
 }
+
+// variables to final config
 
 const mapProps = state => ({
   items: state.sideBar.items,
   isOpen: state.sideBar.isOpen
 })
+
+const styles = {
+  drawer: {
+    width: 250,
+    height: '100%'
+  }
+}
+
+// final config to export
 
 SideBar.propTypes = {
   isOpen: PropTypes.bool.isRequired,
@@ -46,4 +69,4 @@ SideBar.propTypes = {
 export default connect(mapProps, { 
   fetchSideBarItems, 
   toogleSideBar 
-})(SideBar);
+})(withStyles(styles)(SideBar));
